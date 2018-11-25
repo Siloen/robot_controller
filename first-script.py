40cc86cf24a7fc1d8e709d17e424237760cc91ab
siloen <893557024@qq.com>
2018-11-25 10:48:40 +0800

    first

--- first-script.py
+++ first-script.py
@@ -1,1 +1,18 @@
 print('Congratulations on running this script!!')
+
+name=input('this is a new world:' )
+
+print(name)
+
+result=eval(input('please input a expression:'))
+
+print(result)
+
+num=input('please input a num')
+
+num+=10
+
+# 需要转换 
+num=float(num)
+
+print('hello' * 9)
