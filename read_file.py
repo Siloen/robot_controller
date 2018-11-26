a36640a7455736b4e2b991eff176077753411bce
siloen <893557024@qq.com>
2018-11-26 10:34:16 +0800

    install123

--- read_file.py
+++ read_file.py
@@ -1,6 +1,14 @@
+import first-script as fs
+
 f=open("ss.txt","a")
 r=f.write("\n this is a shit")
 f.close()
 
+# with 自带close
+with open("ss.txt","a") as f
 
+if __name__=="__main__":
+
+
+# pip install -r requirments.txt  安装requirements.txt中的第三方库
 
